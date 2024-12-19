6a5c1e03e0d573c84bc57ed9ef7077cdefb471c0
Nora <nely23@fpsanturtzilh.eus>
2024-12-19 13:09:16 +0100

    Last commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,20 +3,36 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
     path: '',
-    redirectTo: 'tab2',
-    pathMatch: 'full'
+    redirectTo: 'home', // Redirige al primer tab
+    pathMatch: 'full',
   },
   {
-    path: 'tab2',
-    loadChildren: () => import('./tab2/tab2.module').then( m => m.Tab2PageModule)
+    path: 'tabs',
+    children: [
+      {
+        path: 'home',
+        loadChildren: () =>
+          import('./home/home.module').then((m) => m.HomePageModule),
+      },
+      {
+        path: 'tab2',
+        loadChildren: () =>
+          import('./tab2/tab2.module').then((m) => m.Tab2PageModule),
+      },
+      {
+        path: 'tab3',
+        loadChildren: () =>
+          import('./tab3/tab3.module').then((m) => m.Tab3PageModule),
+      },
+      {
+        path: '',
+        redirectTo: '/home', // Redirige al tab de "Home" por defecto
+        pathMatch: 'full',
+      },
+    ],
   },
 ];
-
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
